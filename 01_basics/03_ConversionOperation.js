5580078a8c95c4045fb8392ab751771a9cf116cb
Abhinavmishra1105 <mishraabhinav87966@gmail.com>
2026-07-23 14:31:05 +0000

    Conversion in Progress

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+// let age = 11 //
+
+// console.log ( typeof age); //
+// console.log (typeof (age)); //
+
+let age = "33"
+
+console.log ( typeof age);
+console.log (typeof (age)); 
+
+let ValueInNumber = Number(age)
+console.log (  ValueInNumber );
+
+
+
+// "33" => output = 33
+// "33abc" => output =  NaN (NOT a NUMBER)
+// true => 1 ; False => 0 
