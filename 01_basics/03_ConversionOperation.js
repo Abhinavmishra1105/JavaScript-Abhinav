7594bffde195b9e7c8d4038969be052e2224238a
Abhinavmishra1105 <mishraabhinav87966@gmail.com>
2026-07-24 14:44:00 +0000

    Conversion of DataTypes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -5,14 +5,31 @@
 
 let age = "33"
 
-console.log ( typeof age);
-console.log (typeof (age)); 
+// console.log ( typeof age);
+// console.log (typeof (age)); 
 
-let ValueInNumber = Number(age)
-console.log (  ValueInNumber );
-
+let ValueInNumber = Number (age)
+// console.log ( typeof ValueInNumber );
+// console.log ( ValueInNumber );
 
 
 // "33" => output = 33
 // "33abc" => output =  NaN (NOT a NUMBER)
 // true => 1 ; False => 0 
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log (booleanIsLoggedIn);
+
+// 1 => True ; 0 => False 
+// "" => False
+// "Abhinav" => True
+
+
+let SomeNumber = 33
+
+let StringNumber = String (SomeNumber ) 
+console.log( StringNumber);
+console.log( typeof StringNumber);
+
