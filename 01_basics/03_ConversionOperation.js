82f6000e74610becd45f89933ec290a8d8eed453
Abhinavmishra1105 <mishraabhinav87966@gmail.com>
2026-07-25 15:02:32 +0000

    Conversion Operation Completed

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -30,6 +30,42 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let SomeNumber = 33
 
 let StringNumber = String (SomeNumber ) 
-console.log( StringNumber);
-console.log( typeof StringNumber);
+//console.log( StringNumber);
+//console.log( typeof StringNumber);
 
+// ****************** OPERATONS ******************** //
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2/2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Abhinav"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//console.log("1"+ 2);
+//console.log(1+ "2");
+//console.log(1 + 2);
+//console.log("1"+ 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log("1"+ 2 + "2");
+//console.log( 1+ 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+// gameCounter++;
+ ++gameCounter;
+console.log(gameCounter);
