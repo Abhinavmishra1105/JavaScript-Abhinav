9a19a25305fdd5bbf6e3d150545d96c1b8866f6e
Abhinavmishra1105 <mishraabhinav87966@gmail.com>
2026-07-27 14:24:16 +0000

    Datatypes Summary

---
+++ 01_basics/022_datatypes-summary.js
@@ -0,0 +1,37 @@
+// DATATYPES ARE OF 2 TYPES : 1) Primitive & 2) Non - Primitive
+
+// Primitive : 
+// 7 Types :  Number , String , Boolean , null , undefined , BigInt , Symbol
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null 
+// let userEmail ;  
+
+// const Id = Symbol ('123')
+// const anotherId = Symbol ('123')
+
+// console.log(Id === anotherId);
+
+// const bigNumber = 23456787654323456n
+
+// Non-Primitive (Refernce) : 
+// Array , Objects , functions
+
+// const Heros = ["shaktiman" , "Batman" , "SpiderMan" , "Hulk"] // Array
+
+// let myObj = {
+//     name : "Abhinav", 
+//     age : 21,
+// }                             // Object
+
+// const myFunction = Function(){
+//     console.log("Hello World");  
+// }                             // Function
+
+// console.log(typeof outsideTemp ); // Typeof helps to verify datatypes
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
